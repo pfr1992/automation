91a217583fce0be5111570fda5d90ff487f86758
pfr1992 <paulofernando1992@gmail.com>
2023-12-15 10:15:40 -0300

    Update base1.py

--- base1.py
+++ base1.py
@@ -2,6 +2,6 @@ from seleniumbase import Driver
 
 driver = Driver(uc=True)
 #driver.get("https://canal360i.cloud.itau.com.br/login/iparceiros")
-chrome://version/
+driver.get("chrome://version/")
 driver.save_screenshot("valeuveiogarcon.png")
 driver.close()
