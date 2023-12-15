da3cc4d44b01430636695ddf4cf9327019e73983
pfr1992 <paulofernando1992@gmail.com>
2023-12-15 19:13:50 -0300

    Update base1.py

--- base1.py
+++ base1.py
@@ -1,7 +1,7 @@
 from seleniumbase import Driver
 
 DIR ='/home/paulofernando1992/chromedata'
-driver = Driver(uc=True, headless=False, headed=True, user_data_dir='/home/paulofernando1992/chromedata')
+driver = Driver(uc=True, headless=False, headed=True
 #user_data_dir='/home/paulofernando1992/chromedata'
 #driver.get("https://canal360i.cloud.itau.com.br/login/iparceiros")
 driver.get("chrome://version/")
